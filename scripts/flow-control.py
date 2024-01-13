ec0e5fc60c4bf5c2acac90b2457d0836063fd172
pabcrudel <pabcrudel@gmail.com>
2024-01-13 14:01:15 +0100

    add: Input a number and tell the user what type of number it is

---
+++ scripts/flow-control.py
@@ -0,0 +1,16 @@
+# Input a number
+num = input('Input a number?\n')
+
+# Casting the response to number
+num = float(num)
+
+# Get the number type
+if num > 0:
+  numType = 'positive'
+elif num == 0:
+  numType = 'zero'
+else:
+  numType = 'negative'
+
+# Tell the user what type of number it is
+print(num, 'is', numType)
