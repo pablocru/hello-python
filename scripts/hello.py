b6598a24a02df94fd5e3717e14c0c1db050d3d84
pabcrudel <pabcrudel@gmail.com>
2024-01-13 13:39:02 +0100

    add: Input a name and print it on the terminal

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,2 +1,6 @@
 # Greet on the terminal
-print ('Hello, world!')
+print ('Hello, world!\n')
+
+# Input a name and print it on the terminal
+name = input('What is your name?\n')
+print ('Hi, %s.' % name)
