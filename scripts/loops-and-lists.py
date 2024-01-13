1882065424c13512f65282a120eea69c87fffcad
pabcrudel <pabcrudel@gmail.com>
2024-01-13 14:07:06 +0100

    add: Create a list of fruits, add a new one and print them

---
+++ scripts/loops-and-lists.py
@@ -0,0 +1,12 @@
+# List of fruits
+fruits = ['Mango', 'Banana', 'Orange']
+
+# Print each fruit of the list
+for fruit in fruits:
+  print(fruit)
+
+# Add a new fruit to the list
+fruits.append('Apple')
+
+# Print the list with the new one
+print('New list:', fruits)
